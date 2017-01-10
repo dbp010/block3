66f0cb7f67d45bddfaa27f5fc01227066f2bed4e
dbp010 <dbp010@dione>
2017-01-10 16:18:08 +0100

    initial commit

---
+++ src/main/java/de/unidue/inf/is/GoTServlet.java
@@ -0,0 +1,40 @@
+package de.unidue.inf.is;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.unidue.inf.is.utils.DBUtil;
+
+/**
+ * Das könnte die Eintrittsseite sein.
+ */
+public final class GoTServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		final String databaseToCheck = "mygot";
+		boolean databaseExists = DBUtil.checkDatabaseExists(databaseToCheck);
+
+		request.setAttribute("db2name", databaseToCheck);
+
+		if (databaseExists) {
+//			request.setAttribute("db2exists", "vorhanden! Supi!");
+			System.out.println("Datenbank " + databaseToCheck + "vorhanden! Supi!");
+		}
+		else {
+//			request.setAttribute("db2exists", "nicht vorhanden :-(");
+			System.out.println("Datenbank " + databaseToCheck + "nicht vorhanden :-(");
+		}
+
+		request.getRequestDispatcher("got_start.ftl").forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/got_start.ftl
@@ -0,0 +1,66 @@
+<html>
+<head><title>GoT Datenbank</title>
+<style type="text/css">
+* {
+   margin:0;
+   padding:0;
+}
+
+body{
+   text-align:center;
+   background: #efe4bf none repeat scroll 0 0;
+}
+
+#wrapper{
+   width:960px;
+   margin:0 auto;
+   text-align:left;
+   background-color: #fff;
+   border-radius: 0 0 10px 10px;
+   padding: 20px;
+   box-shadow: 1px -2px 14px rgba(0, 0, 0, 0.4);
+}
+
+#site{
+    background-color: #fff;
+    padding: 20px 0px 0px 0px;
+}
+.centerBlock{
+	margin:0 auto;
+}
+</style>
+
+<body>
+	<div id="wrapper">
+	    <div id="logo">
+			<img width="100%" src="images/header.jpg" class="centerBlock" />
+		</div>
+		<div id="site">
+		
+		<#--
+		<p>
+			Die Datenbank "${db2name}" ist ${db2exists}
+		</p>
+		-->
+		
+		</div>
+		
+		<div id="figures">
+		<h1>Figuren</h1>
+		</div>
+		
+		<div id="houses">
+		<h1>Häuser</h1>
+		</div>
+		
+		<div id="seasons">
+		<h1>Staffeln</h1>
+		</div>
+		
+		<div id="playlists">
+		<h1>Playlisten</h1>
+		</div>
+		
+	</div>
+</body>
+</html>
